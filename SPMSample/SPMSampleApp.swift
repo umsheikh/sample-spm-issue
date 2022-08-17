ab13d8868dba9275641b2cfa4a285f0e65307907
Usman Sheikh <usman@Usmans-Air.attlocal.net>
2022-08-17 08:52:28 -0500

    initial commit

---
+++ SPMSample/SPMSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPMSampleApp.swift
+//  SPMSample
+//
+//  Created by Usman Sheikh on 8/17/22.
+//
+
+import SwiftUI
+
+@main
+struct SPMSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
